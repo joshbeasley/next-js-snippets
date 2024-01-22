8d50fb807cbbdf9ffc8aac3634f8e97810831e1f
Josh Beasley <joshua.beasley@km-sscdev.org>
2024-01-21 19:20:09 -0800

    render snippets on home page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,12 @@
+import { db } from '@/db'
 import Image from 'next/image'
 
-export default function Home() {
-  return <div>Home Page</div>
+export default async function Home() {
+  const snippets = await db.snippet.findMany()
+
+  const renderedSnippets = snippets.map((snippet) => {
+    return <div key={snippet.id}>{snippet.title}</div>
+  })
+
+  return <div>{renderedSnippets}</div>
 }
